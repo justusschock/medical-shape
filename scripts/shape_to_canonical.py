c8278a0cf3c55a94ffd3c79a8fb4307eacc3b964
Justus Schock <justus.schock@posteo.de>
2022-01-21 13:57:57 +0100

    Update logging & ToCanonical

---
+++ scripts/shape_to_canonical.py
@@ -0,0 +1,50 @@
+import json
+
+import numpy as np
+import torch
+import torchio as tio
+
+from shape import Shape, ShapeSupportSubject
+from shape.transforms import ToCanonical
+
+
+def to_canonical(in_path, out_path, image_path=None, out_path_image=None):
+    if in_path.endswith(".json"):
+        with open(in_path) as f:
+            points = torch.tensor(json.load(f))
+        points = torch.flip(points, (-1,))
+        shape = Shape(tensor=points)
+    else:
+        shape = Shape(in_path)
+
+    if image_path is not None:
+        image = tio.Image(image_path)
+        shape.affine = image.affine.copy()
+    else:
+        image = None
+    canonical = ToCanonical("image")(ShapeSupportSubject(shape=shape, image=image))
+    shape = canonical["shape"]
+    if image is not None and out_path_image is not None:
+        canonical["image"].affine = np.eye(4)
+        canonical["image"].save(out_path_image)
+    shape.save(out_path)
+    if image is not None:
+        return shape, image
+    return shape
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Convert shape to canonical coordinates.")
+    parser.add_argument("in_path", help="Path to input shape file.")
+    parser.add_argument("out_path", help="Path to output shape file.")
+    parser.add_argument("--image_path", help="Path to image file.")
+    parser.add_argument("--out_path_image", help="Path to output image file.")
+    args = parser.parse_args()
+    res = to_canonical(
+        args.in_path,
+        args.out_path,
+        args.image_path,
+        args.out_path_image,
+    )
--- shape/transforms/to_canonical.py
+++ shape/transforms/to_canonical.py
@@ -1,41 +1,71 @@
+from typing import Optional
+
 import nibabel as nib
 import numpy as np
 import torch
 import torchio as tio
-from rising.transforms.functional.affine import affine_point_transform
+from rising.utils.affine import points_to_cartesian, points_to_homogeneous
 
 from shape.subject import ShapeSupportSubject
 from shape.transforms.mixin import TransformShapeValidationMixin
 
 
-class ToCanonical(
-    tio.transforms.preprocessing.ToCanonical, TransformShapeValidationMixin
-):
-    def apply_transform(self, subject: tio.data.Subject) -> tio.data.Subject:
-        sub = dict(
-            super().apply_transform(
-                getattr(subject, "get_images_only_subject", lambda: subject)()
-            )
-        )
+class ToCanonical(tio.transforms.preprocessing.ToCanonical, TransformShapeValidationMixin):
+    def __init__(self, shape_image_key: Optional[str] = None, *args, **kwargs):
+        super().__init__(*args, parse_input=False, **kwargs)
+        self.shape_image_key = shape_image_key
 
-        for k, v in getattr(subject, "get_shapes_dict", lambda: {})().items():
+    def apply_transform(self, subject: tio.data.Subject) -> tio.data.Subject:
+
+        shapes_dict = getattr(subject, "get_shapes_dict", lambda: {})()
+        if shapes_dict:
+            if isinstance(self.shape_image_key, str):
+                shape_image_key = self.shape_image_key
+            elif self.shape_image_key is None and len(subject.get_images_dict(intensity_only=False)) == 1:
+                shape_image_key = list(subject.get_images_dict(intensity_only=False).keys())[0]
+            else:
+                raise ValueError(
+                    f"shape_image_key must be a string or None if there is only one image in the subject. Got {self.shape_image_key}"
+                )
+
+            src_ref_shape = torch.tensor(subject[shape_image_key].spatial_shape)
+
+        new_sub = {}
+        try:
+            new_sub.update(
+                dict(super().apply_transform(getattr(subject, "get_images_only_subject", lambda: subject)()))
+            )
+        except ValueError:
+            new_sub.update(ShapeSupportSubject.exclude_shapes(dict(subject)))
+
+        if shapes_dict:
+            dst_ref_shape = torch.tensor(new_sub[shape_image_key].spatial_shape)
+
+        for k, v in shapes_dict.items():
+
             affine_pre = v.affine
             if nib.aff2axcodes(affine_pre) == tuple("RAS"):
-                sub[k] = v
+                new_sub[k] = v
                 continue
-            # some dummy array to make use of nibabel
-            array = np.zeros((2, 2, 2, 1, 1))  # (W, H, D, 1, C)
-            nii = nib.Nifti1Image(array, affine_pre)
-            reoriented = nib.as_closest_canonical(nii)
-            affine_post = reoriented.affine
 
-            trafo = torch.tensor(
-                affine_pre, dtype=torch.float
-            ).inverse() @ torch.tensor(affine_post, dtype=torch.float)
+            else:
+                affine_post = affine_pre.dot(
+                    nib.orientations.inv_ornt_aff(
+                        nib.orientations.io_orientation(affine_pre),
+                        src_ref_shape.numpy(),
+                    )
+                )
 
-            transformed_points = affine_point_transform(v.tensor[None], trafo[None])[0]
-            v.set_data(transformed_points)
-            v.affine = affine_post
-            sub[k] = v
+                hom_points = points_to_homogeneous(v.tensor[None])[0].numpy()
+                trafo = np.linalg.inv(affine_post).dot(affine_pre)
+                points_transformed = []
+                for x in hom_points:
+                    points_transformed.append(trafo.dot(x))
 
-        return type(subject)(sub)
+                points_transformed = points_to_cartesian(torch.from_numpy(np.array(points_transformed))[None])[0]
+
+                v.set_data(points_transformed)
+                v.affine = affine_post
+                new_sub[k] = v
+
+        return type(subject)(new_sub)
