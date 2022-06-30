e4e9618ac3a551980a32515024b7bb4ccd9f27e0
Justus Schock <12886177+justusschock@users.noreply.github.com>
2022-06-30 19:35:19 +0200

    Update to_canonical.py

--- medical_shape/transforms/to_canonical.py
+++ medical_shape/transforms/to_canonical.py
@@ -1,8 +1,8 @@
 from medical_shape.transforms.to_orientation import ToOrientation
-from typing import Optional, Union, Tuple, List
+from typing import Optional, Tuple
 
 class ToRAS(ToOrientation):
-    def __init__(self, shape_trafo_image_size: Optional[Union[Tuple[int, int, int], List[int]]] = None, shape_trafo_image_key: Optional[str] = None, **kwargs):
+    def __init__(self, shape_trafo_image_size: Optional[Tuple[int, int, int]] = None, shape_trafo_image_key: Optional[str] = None, **kwargs):
         super().__init__(axcode=('R', 'A', 'S'), shape_trafo_image_key=shape_trafo_image_key, shape_trafo_image_size=shape_trafo_image_size)
 
 ToCanonical = ToRAS
