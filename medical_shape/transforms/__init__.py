aca1306820a4cb22cb4b2458df2823838d30e2cc
Justus Schock <12886177+justusschock@users.noreply.github.com>
2022-07-28 15:58:26 +0200

    Prune transforms (#9)
    * Delete affine.py
    
    * Delete anisotropy.py
    
    * Delete crop_or_pad.py
    
    * Delete crop_or_pad_per_image.py
    
    * Delete crop.py
    
    * Delete normalization.py
    
    * Delete pad.py
    
    * Delete rescale_intensity.py
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * update init
    
    * update mdlu references
    
    * update tests

--- medical_shape/transforms/__init__.py
+++ medical_shape/transforms/__init__.py
@@ -1,29 +1,12 @@
-from medical_shape.transforms.affine import Affine, RandomAffine
-from medical_shape.transforms.anisotropy import RandomAnisotropy
 from medical_shape.transforms.copy_affine import CopyAffine
-from medical_shape.transforms.crop import Crop
-from medical_shape.transforms.crop_or_pad import CropOrPad
-from medical_shape.transforms.crop_or_pad_per_image import CropOrPadPerImage
 from medical_shape.transforms.mixin import TransformShapeValidationMixin
-from medical_shape.transforms.normalization import NNUnetNormalization
-from medical_shape.transforms.pad import Pad
 from medical_shape.transforms.resample import Resample
-from medical_shape.transforms.rescale_intensity import RescaleIntensityPercentiles
 from medical_shape.transforms.to_canonical import ToCanonical, ToRAS
 from medical_shape.transforms.to_orientation import ToOrientation
 
 __all__ = [
-    "Affine",
     "CopyAffine",
-    "Crop",
-    "CropOrPad",
-    "CropOrPadPerImage",
-    "NNUnetNormalization",
-    "Pad",
-    "RandomAffine",
-    "RandomAnisotropy",
     "Resample",
-    "RescaleIntensityPercentiles",
     "ToCanonical",
     "ToOrientation",
     "ToRAS",
--- medical_shape/transforms/rescale_intensity.py
+++
@@ -1,8 +0,0 @@
-from mdlu.transforms import RescaleIntensityPercentiles as _RescaleIntensityPercentiles
-
-from medical_shape.transforms.mixin import TransformShapeValidationMixin
-
-
-# Need to subclass from TransformShapeValidationMixin as that one fixes kwargs
-class RescaleIntensityPercentiles(_RescaleIntensityPercentiles, TransformShapeValidationMixin):
-    pass
