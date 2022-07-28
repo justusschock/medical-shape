9efb608f443a006a51b600f225b29d80d4cc6158
Justus Schock <12886177+justusschock@users.noreply.github.com>
2022-07-28 14:57:34 +0200

    Delete normalization.py

--- medical_shape/transforms/normalization.py
+++
@@ -1,8 +0,0 @@
-from mdlu.transforms import NNUnetNormalization as _NNUnetNormalization
-
-from medical_shape.transforms.mixin import TransformShapeValidationMixin
-
-
-# Need to subclass from TransformShapeValidationMixin as that one fixes kwargs
-class NNUnetNormalization(_NNUnetNormalization, TransformShapeValidationMixin):
-    pass
